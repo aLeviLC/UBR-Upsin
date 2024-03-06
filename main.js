148d6b810375360048a0d2e481c1c9aa809109ec
ChochiasG <josias04102003@gmail.com>
2024-03-06 10:25:11 -0700

    commit pacientes

--- main.js
+++ main.js
@@ -11,7 +11,33 @@ document.getElementById('iniciarSesion').addEventListener('click', async (e) => 
     window.location.href = './src/pages/pacientes.html';
     console.log(credentials);
   } catch (error) {
-    alert("Usuario o contraseña incorrectos!");
+    Toastify({
+      text: "Usuario o Contraseña invalidos!",
+      close: true,
+      duration: 3000,
+      gravity: "top",
+      position: "right",
+      style: { 
+          display: "flex", 
+          justifyContent: "space-between", 
+          background: "#cb4335", 
+          borderRadius: "10px", 
+          width: "25rem" 
+      },
+    }).showToast();
+    console.log(error);
   }
   
+});
+
+document.querySelector('.forget-pass').addEventListener('click', (e) => {
+  document.querySelector('.scrim').classList.remove('is-hidden');
+});
+
+document.querySelector('.close').addEventListener('click', (e) => {
+  document.querySelector('.scrim').classList.add('is-hidden');
+});
+
+document.querySelector('.scrim').addEventListener('click', (e) => {
+  document.querySelector('.scrim').classList.add('is-hidden');
 });
